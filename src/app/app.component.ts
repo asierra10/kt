7672b0041079e92d67ff057c5d4887a4508d0fd6
46047018 <46047018@claro.com.co>
2024-08-23 23:23:26 -0500

    Cambios responsivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
+import { LiveComponent } from './components/live/live.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    FooterComponent
+    FooterComponent,
+    LiveComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'kt';
+
+  constructor(private router: Router) {}
+
+  navigateToLive() {
+    console.log("aaaaaaaaaaaaaaa")
+    this.router.navigate(['/live']);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LiveComponent } from './components/live/live.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    //{ path: '', component: AppComponent },
+    { path: 'live', component: LiveComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
