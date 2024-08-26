e6264f3509a0f90fa2215fbe1598e51af4d75f88
46047018 <46047018@claro.com.co>
2024-08-26 15:54:33 -0500

    Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { LiveComponent } from './components/live/live.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
+    MenuComponent,
+    BannerComponent,
     FooterComponent,
     LiveComponent
   ],
